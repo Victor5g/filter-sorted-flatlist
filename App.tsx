edefc9ddbd054b0bd9a8e9d02baedde8a4355021
Victor5g <vicoe.sales@gmail.com>
2022-05-29 11:26:36 -0300

    Feat: search and filter

---
+++ App.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  View,
+  TextInput,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import ListItem from './src/components/ListItem';
+
+import listUsers from './mock/api/usersResponse';
+
+const App = () => {
+  const [searchText, setSearchText] = useState<string>('');
+  const [list, setList] = useState(listUsers);
+
+  useEffect(()=>{
+    if(emptyList()){
+      setList(listUsers);
+      return
+    }
+    searchUser();
+  },[searchText])
+  
+   const emptyList = () =>{
+     return searchText === ''? true: false
+   };
+
+   const searchUser = () =>{
+     setList(
+     listUsers.filter(item =>{
+      return (item.name.toLowerCase().indexOf(searchText.toLowerCase()) > -1);
+     })
+     )
+  };
+
+   const handleOrderClick = () => {
+     let newlistUsers = [...list];
+     newlistUsers.sort((indexA, indexB)=>
+     (indexA.name > indexB.name)? 1: (indexB.name> indexA.name)? -1 :0)
+     setList(newlistUsers);
+  };
+  
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchArea}>
+        <TextInput
+          style={styles.input}
+          placeholder="Pesquise um usuario"
+          placeholderTextColor="#888"
+          value={searchText}
+          onChangeText={(t) => setSearchText(t)}
+        />
+        <TouchableOpacity onPress={handleOrderClick} style={styles.orderButton}>
+          <MaterialCommunityIcons
+            name="order-alphabetical-ascending"
+            size={32}
+            color="#888"
+          />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={list}
+        style={styles.list}
+        renderItem={({ item }) => <ListItem data={item} />}
+        keyExtractor={(item) => item.id}
+      />
+
+      <StatusBar style="light" />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#242425',
+  },
+  input: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#363636',
+    margin: 30,
+    borderRadius: 5,
+    fontSize: 19,
+    paddingLeft: 15,
+    paddingRight: 15,
+    color: '#FFFFFF',
+  },
+  searchArea: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  orderButton: {
+    width: 32,
+    marginRight: 30,
+  },
+  list: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ mock/api/usersResponse.ts
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'Maria Eduarda',
+    email: 'duda.martins34@gmail.com',
+    avatar: 'https://i.pravatar.cc/150?img=38',
+  },
+  {
+    id: 2,
+    name: 'Ana Julia',
+    email: 'Anajulia1233@outlook.com',
+    avatar: 'https://i.pravatar.cc/150?img=45',
+  },
+  {
+    id: 3,
+    name: 'Gustavo Medeiros',
+    email: 'GM@hotmail.com',
+    avatar: 'https://i.pravatar.cc/150?img=55',
+  },
+  {
+    id: 4,
+    name: 'Marcos Junior',
+    email: 'MJJunior@icloud.com',
+    avatar: 'https://i.pravatar.cc/150?img=12',
+  },
+  {
+    id: 5,
+    name: 'Thaneya Mayara',
+    email: 'ThaMay@hotmail.com',
+    avatar: 'https://i.pravatar.cc/150?img=49',
+  },
+  {
+    id: 6,
+    name: 'Alessandre martins',
+    email: 'AleeMartsa@gmail.com',
+    avatar: 'https://i.pravatar.cc/150?img=23',
+  },
+  {
+    id: 7,
+    name: 'Caroline braganca',
+    email: 'Caroli34Brag@gmail.com',
+    avatar: 'https://i.pravatar.cc/150?img=34',
+  },
+];
